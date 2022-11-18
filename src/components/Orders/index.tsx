027b8b318919599c417927be3849d2cd5ee7c8dc
Marcos Júnior <mjworks2020@outlook.com>
2022-11-18 18:24:30 -0300

    Added new updates

---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,15 @@
+import { Board, Container } from "./styles";
+
+export function Orders() {
+    return (
+        <Container>
+            <Board>
+                <header>
+                    <span>🕐</span>
+                    <strong>Fila de espera</strong>
+                    <span>(1)</span>
+                </header>
+            </Board>
+        </Container>
+    )
+}
