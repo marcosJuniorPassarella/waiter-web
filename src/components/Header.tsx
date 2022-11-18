81ed559eee3a5cdb9fae86b372da622eadc07a0d
Marcos Júnior <mjworks2020@outlook.com>
2022-11-18 08:36:11 -0300

    Create project

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header>
+            <div className="page-details">
+                <h1>Pedidos</h1>
+                <h2>Acompanhe os pedidos dos clientes</h2>
+            </div>
+        </header>
+    );
+}
