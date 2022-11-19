ab7ddce9497df607daa731bf8e01399dcb2f3cb0
Marcos Júnior <mjworks2020@outlook.com>
2022-11-18 21:49:22 -0300

    Added new updates

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,15 +1,62 @@
-import { Board, Container } from "./styles";
+import { Board, Container, OrdersContainer } from "./styles";
 
 export function Orders() {
-    return (
-        <Container>
-            <Board>
-                <header>
-                    <span>🕐</span>
-                    <strong>Fila de espera</strong>
-                    <span>(1)</span>
-                </header>
-            </Board>
-        </Container>
-    )
+  return (
+    <Container>
+      <Board>
+        <header>
+          <span>🕐</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Itens</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+      <Board>
+        <header>
+          <span>🕐</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Itens</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+      <Board>
+        <header>
+          <span>🕐</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Itens</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+    </Container>
+  );
 }
--- src/components/Orders/styles.ts
+++ src/components/Orders/styles.ts
@@ -4,8 +4,15 @@ export const Container = styled.div`
     width: 100%;
     max-width: 1216px;
     margin: 40px auto;
+    display: flex;
+    gap: 32px;
 `
 export const Board = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+flex: 1;
 padding: 16px;
 border: 1px solid rgba(204, 204, 204, 0.4);
 border-radius: 16px;
@@ -16,5 +23,36 @@ header {
     display: flex;
     align-items: center;
     gap: 8px;
+}
+`
+
+export const OrdersContainer = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+margin-top: 24px;
+button {
+    background: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    height: 129px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+
+    strong {
+        font-weight: 500;
+    }
+
+    span{
+        font-size: 14px;
+        color: #666;
+    }
+
+    & + button {
+        margin-top: 24px;
+    }
 }
 `
