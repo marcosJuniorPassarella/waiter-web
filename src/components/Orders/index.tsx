c4d54ad1ac7ddb94687765c8c3a8afc39013d881
Marcos Júnior <mjworks2020@outlook.com>
2022-11-21 18:29:44 -0300

    Added new alterations

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,12 +1,32 @@
 import { Container } from "./styles";
 import { OrdersBoard } from "../OrdersBoard/index";
+import { Order } from "../../types/Order";
+
+const orders = [
+  {
+    _id: "637574f256672bca8e8bd0f7",
+    table: "123",
+    status: "WAITING",
+    products: [
+      {
+        product: {
+          name: "Pizza Quatro Queijos",
+          imagePath: "1668547752493-quatro-queijos.png",
+          price: 40,
+        },
+        quantity: 3,
+        _id: "637574f256672bca8e8bd0f8",
+      },
+    ],
+  },
+];
 
 export function Orders() {
   return (
     <Container>
-      <OrdersBoard />
-      <OrdersBoard />
-      <OrdersBoard />
+      <OrdersBoard orders={orders} icon="🕑" title="Fila de espera" />
+      <OrdersBoard orders={[]} icon="👨‍🍳" title="Em preparação" />
+      <OrdersBoard orders={[]} icon="✅" title="Pronto" />
     </Container>
   );
 }
--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,24 +1,35 @@
+import { Order } from "../../types/Order";
 import { Board, OrdersContainer } from "./styles";
 
-export function OrdersBoard() {
-    return (
-        <Board>
-        <header>
-          <span>🕐</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
+interface OrdersBoardProps {
+  icon: string;
+  title: string;
+  orders: Order[];
+}
 
+export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+  function handleOpenModal() {
+    alert('Modal')
+  }
+
+  return (
+    <Board>
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>({orders?.length})</span>
+      </header>
+
+      {orders.length > 0 && (
         <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
+          {orders.map((e) => (
+            <button onClick={handleOpenModal} type="button" key={e?._id}>
+              <strong>Mesa {e?.table} </strong>
+              <span>{e?.products.length} itens</span>
+            </button>
+          ))}
         </OrdersContainer>
-      </Board>
-    )
+      )}
+    </Board>
+  );
 }
